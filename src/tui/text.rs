264fd53ebcf3922ec65325fac2753c54ebcea7a7
Sagan Sutherland <sagan@farthinghalearms.com>
2023-04-11 12:14:25 -0400

    start of a text module

---
+++ src/tui/text.rs
@@ -0,0 +1,11 @@
+use super::{Frame, Rect, Render};
+
+pub struct Text {
+    text: String,
+}
+
+impl Render for Text {
+    fn render(&self, frame: &mut Frame, region: Rect) -> std::io::Result<()> {
+        todo!()
+    }
+}
