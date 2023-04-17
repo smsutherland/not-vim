af4eee102451bc9756043e14cfa6f5df311ff4a5
smsutherland <sagan@farthinghalearms.com>
2023-04-16 20:45:49 -0400

    Make Text hold separate lines

--- src/tui/text.rs
+++ src/tui/text.rs
@@ -6,31 +6,24 @@
 use super::{Frame, Rect, Render};
 
 /// A piece of text which can be drawn to the terminal.
-pub struct Text {
+pub struct Text<'a> {
     /// The content of the [`Text`].
-    text: String,
+    lines: &'a [String],
 }
 
-impl Render for Text {
+impl Render for Text<'_> {
     fn render(&self, frame: &mut Frame, region: Rect) {
-        let mut x = 0;
-        let mut y = 0;
-        for c in self.text.chars() {
-            if c == '\n' {
-                y += 1;
-                x = 0;
-            } else {
+        for (y, line) in self.lines.iter().take(region.height as usize).enumerate() {
+            for (x, c) in line.chars().take(region.width as usize).enumerate() {
+                let (x, y) = (x as u16, y as u16);
                 frame.set_char(c, x + region.left, y + region.top);
-                x += 1;
             }
         }
     }
 }
 
-impl From<&[String]> for Text {
-    fn from(value: &[String]) -> Self {
-        Self {
-            text: value.join("\n"),
-        }
+impl<'a> From<&'a [String]> for Text<'a> {
+    fn from(value: &'a [String]) -> Self {
+        Self { lines: value }
     }
 }
